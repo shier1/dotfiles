b402c250092134a5b60088773a72ec5951983e26
shier1 <www.zyanping14@gmail.com>
2023-07-24 23:41:25 +0800

    update hyprland config

--- update_the_configure.sh
+++ update_the_configure.sh
@@ -1,4 +1,8 @@
 cp -r ~/.config/hypr .
+echo "[success] copy the ~/.config/hypr ."
 cp -r ~/.config/waybar .
+echo "[success] copy ~/.config/waybar ."
 cp -r ~/.config/alacritty .
+echo "[success] copy ~/.config/alacritty ."
 cp -r ~/.config/nvim .
+echo "[success] copy ~/.config/nvim ."
